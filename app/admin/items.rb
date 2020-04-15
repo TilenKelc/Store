8b686c2e18104195f401b900e6e3c8051437dfb7
lenartgolob <lenart.golob17@gmail.com>
2020-04-15 11:09:35 +0200

    active admin

---
+++ app/admin/items.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Item do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :desc, :stock, :price
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :desc, :stock, :price]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
