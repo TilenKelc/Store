a91d882da3b3c6fd211f3f17dca5b17fca0d6290
Tilen <tilen.kelc@github.com>
2020-04-24 19:16:46 +0200

    Fixing order edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :orders, only: [:index, :show, :edit, :create]
+  resources :orders, only: [:index, :show, :edit, :create, :update]
   resources :items
   devise_for :customers
   root to: "items#index"
